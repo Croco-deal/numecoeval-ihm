d64d4b9e5b97f9c9a39322197417814979c6be6b
Ahmed BEN SALHA <abensalha@inops.fr>
2023-05-09 11:03:37 +0200

    add ref page ++ fix landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { TimelineModule } from './timeline/timeline.module';
 import { HistoryModule } from './history/history.module';
+import { ReferencialComponent } from './referencial/referencial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ReferencialComponent,
   ],
   imports: [
     BrowserModule,
